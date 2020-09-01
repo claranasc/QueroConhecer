66a189c8662e3a09a2f119a160ef5864b33f8005
Clara Nascimento <clara.nascimento@outlook.com>
2020-08-31 21:44:38 -0300

    96.1 - Create project, add image in Assets, add image in LaunchScreen

---
+++ QueroConhecer/QueroConhecer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QueroConhecer
+//
+//  Created by Clara Nascimento on 31/08/20.
+//  Copyright © 2020 Clara Nascimento. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
