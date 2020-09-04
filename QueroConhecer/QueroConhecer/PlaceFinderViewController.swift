27065916c312ff644e373c1ee2d269b5519f31ec
Clara Nascimento <clara.nascimento@outlook.com>
2020-09-04 09:14:50 -0300

    96.3 - Add activity indicator; create all outlets to PlaceFinderViewController.

--- QueroConhecer/QueroConhecer/PlaceFinderViewController.swift
+++ QueroConhecer/QueroConhecer/PlaceFinderViewController.swift
@@ -7,14 +7,29 @@
 //
 
 import UIKit
+import MapKit
 
 class PlaceFinderViewController: UIViewController {
 
+    @IBOutlet weak var tfCity: UITextField!
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var aiLoading: UIActivityIndicatorView!
+    @IBOutlet weak var viLoading: UIView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
+
+    @IBAction func findCity(_ sender: UIButton) {
+    }
+    
+    @IBAction func close(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
     
 
     /*
