e5027bc61b289887b13884e556acf65eb9b15a1a
Clara Nascimento <clara.nascimento@outlook.com>
2020-10-07 21:28:57 -0300

    102.2 - Add mapView.delegate; Add extension MapViewController

--- QueroConhecer/QueroConhecer/MapViewController.swift
+++ QueroConhecer/QueroConhecer/MapViewController.swift
@@ -24,6 +24,8 @@ class MapViewController: UIViewController {
         searchBar.isHidden = true
         viInfo.isHidden = true
         
+        mapView.delegate = self
+        
         if places.count == 1 {
             title = places[0].name
         } else {
@@ -52,5 +54,26 @@ class MapViewController: UIViewController {
     }
     
     @IBAction func showSearchBar(_ sender: UIBarButtonItem) {
+    }
+}
+
+extension MapViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if !(annotation is PlaceAnnotation) {
+            return nil
+        }
+        let type = (annotation as! PlaceAnnotation).type
+        let identifier = "\(type)"
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKMarkerAnnotationView
+        if annotationView == nil {
+            annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+        }
+        annotationView?.annotation = annotation
+        annotationView?.canShowCallout = true
+        annotationView?.markerTintColor = type == .place ? UIColor(named: "main") : UIColor(named: "poi")
+        annotationView?.glyphImage = type == .place ? UIImage(named: "placeGlyph") : UIImage(named: "poiGlyph")
+        annotationView?.displayPriority = type == .place ? .required : .defaultHigh
+        
+        return annotationView
     }
 }
